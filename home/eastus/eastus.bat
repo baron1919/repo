7c2ca55ff11134dc772700953c7e7603b7add396
ba3walos88 <mansour.aymen77@gmail.com>
2019-04-15 22:13:32 +0200

    first commit

--- home/eastus/eastus.bat
+++ home/eastus/eastus.bat
@@ -1,6 +1,6 @@
 set deploymentName="r459d0depeastus"
-set subscriptionId="fd19faab-b635-424b-84ae-384249a593ee"
-set resourceGroupName="rg850736"
+set subscriptionId="dd448447-6546-4b97-b3f9-95ca9b8454b2"
+set resourceGroupName="rg474424"
 set resourceGroupLocation="southcentralus"
 
 
